3c85a39015ac6ad643ffab7430bf0d25b55fa651
lalitchilka <lalitchilka@gmail.com>
2020-06-03 19:05:38 +0200

    Adding Initial project files

---
+++ src/main/java/com/myapp/springbootpetclicnicpractice/SpringbootPetClicnicPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.springbootpetclicnicpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPetClicnicPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPetClicnicPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myapp/springbootpetclicnicpractice/SpringbootPetClicnicPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.springbootpetclicnicpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPetClicnicPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
