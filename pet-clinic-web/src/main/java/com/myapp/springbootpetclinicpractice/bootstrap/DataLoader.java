80b0ff64c76e1c6db6bead3b7bb1c2923e0a4037
lalitchilka <lalitchilka@gmail.com>
2020-07-11 17:10:11 +0530

    Added Pet and contact into to owners on startup. Closes #33

--- pet-clinic-web/src/main/java/com/myapp/springbootpetclinicpractice/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/myapp/springbootpetclinicpractice/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.myapp.springbootpetclinicpractice.bootstrap;
 
 import com.myapp.springbootpetclinicpractice.model.Owner;
+import com.myapp.springbootpetclinicpractice.model.Pet;
 import com.myapp.springbootpetclinicpractice.model.PetType;
 import com.myapp.springbootpetclinicpractice.model.Vet;
 import com.myapp.springbootpetclinicpractice.service.OwnerService;
@@ -8,6 +9,8 @@ import com.myapp.springbootpetclinicpractice.service.PetTypeService;
 import com.myapp.springbootpetclinicpractice.service.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -36,12 +39,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Lalit");
         owner1.setLastName("Chilka");
+        owner1.setAddress("1142, Sykes Extension");
+        owner1.setCity("Kolhapur");
+        owner1.setTelephone("8452035525");
+
+        Pet lalitsPet = new Pet();
+        lalitsPet.setPetType(savedCatType);
+        lalitsPet.setName("Catush");
+        lalitsPet.setBirthDate(LocalDate.now());
+        lalitsPet.setOwner(owner1);
+        owner1.getPets().add(lalitsPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Asu");
         owner2.setLastName("Bieganowska");
+        owner2.setAddress("23, Migowska");
+        owner2.setCity("Gdansk");
+        owner2.setTelephone("515566664");
+
+        Pet asusPet = new Pet();
+        asusPet.setPetType(savedDogType);
+        asusPet.setName("Fluffek");
+        asusPet.setBirthDate(LocalDate.now());
+        asusPet.setOwner(owner2);
+        owner1.getPets().add(asusPet);
 
         ownerService.save(owner2);
 
