4de3c1e0d5fbff4ddeec0e6a81488e18fd6cf756
lalitchilka <lalitchilka@gmail.com>
2020-07-11 16:17:51 +0530

    Added PetTypeService. Closes #28

---
+++ pet-clinic-data/src/main/java/com/myapp/springbootpetclinicpractice/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.myapp.springbootpetclinicpractice.service;
+
+import com.myapp.springbootpetclinicpractice.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
